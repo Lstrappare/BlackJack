b9d8d3fe97589bebce9a04ae46c02bfde18e8655
Lstrappare <j.m.cisval1@gmail.com>
2026-01-14 22:32:52 -0600

    Initial Commit

---
+++ BlackJack/BlackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackApp.swift
+//  BlackJack
+//
+//  Created by Jose Cisneros on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
